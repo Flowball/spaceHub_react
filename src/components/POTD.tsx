e6b5244b29ffa5289045002c40c1b5aeae4f2968
oscar.jigbring <oscar.jigbring@hotmail.se>
2024-02-22 16:51:40 +0100

    added style to POTD

--- src/components/POTD.tsx
+++ src/components/POTD.tsx
@@ -15,13 +15,32 @@ function POTD() {
   if (query.isError) return <h1>Something went wrong....</h1>;
 
   return (
-    <div className="flex flex-col gap-4">
-      <h1>Copyright: {query.data.copyright}</h1>
-      <h1>Title: {query.data.title}</h1>
-      <h2>Date: {query.data.date}</h2>
-      <h2>Explanation: {query.data.explanation}</h2>
-      <img src={query.data.hdurl} alt="" />
-    </div>
+    <>
+      <h1 className="flex flex-col items-center text-2xl font-space p-4">
+        PICTURE OF THE DAY
+      </h1>
+      <div className="flex flex-col mx-20 bg-slate-300 rounded-lg p-4">
+        <div className="flex justify-between py-4">
+          <p>
+            Date: <b>{query.data.date}</b>
+          </p>
+          <p>
+            Title: <b>{query.data.title}</b>
+          </p>
+        </div>
+        <div className="flex">
+          <img
+            src={query.data.hdurl}
+            alt="picture of the day"
+            className="max-w-screen-lg rounded-lg"
+          />
+          <h2 className="px-4">Explanation: {query.data.explanation}</h2>
+        </div>
+        <div className="py-4">
+          Copyright: <b>{query.data.copyright}</b>
+        </div>
+      </div>
+    </>
   );
 }
 

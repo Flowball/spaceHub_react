a5a4eba0999b8961ee0737407b7f4e35d9ca6161
oscar.jigbring <oscar.jigbring@hotmail.se>
2024-03-01 10:36:05 +0100

    added .env for api key

--- src/api/spaceX.ts
+++ src/api/spaceX.ts
@@ -1,6 +1,5 @@
 export const getSpaceXData = async () => {
   const response = await fetch("https://api.spacexdata.com/v5/launches");
   const data = await response.json();
-  console.log(data);
   return data;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import react from "@vitejs/plugin-react";
 import stylexPlugin from "unplugin-stylex/vite";
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), stylexPlugin({})],
+  define: {
+    "process.env": process.env,
+  },
 });
